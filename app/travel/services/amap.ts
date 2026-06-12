b6aafd0a92fd1fdafe45a234c32da1692ab04317
Developer <dev@xiequ.local>
2026-06-13 00:27:40 +0800

    feat: amap plugins 添加 AMap.MarkerClusterer

--- app/travel/services/amap.ts
+++ app/travel/services/amap.ts
@@ -56,6 +56,7 @@ export function loadAmap(): Promise<AMapModule> {
         'AMap.DistrictSearch',
         'AMap.Geolocation',
         'AMap.Geocoder',
+        'AMap.MarkerClusterer',
       ],
     }),
   ) as Promise<AMapModule>;
