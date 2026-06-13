738182783676d06c006a7e9950b366769906df27
Developer <dev@xiequ.local>
2026-06-13 17:22:38 +0800

    feat: add AMap.Driving plugin to loadAmap

--- app/travel/services/amap.ts
+++ app/travel/services/amap.ts
@@ -56,6 +56,7 @@ export function loadAmap(): Promise<AMapModule> {
         'AMap.DistrictSearch',
         'AMap.Geolocation',
         'AMap.Geocoder',
+        'AMap.Driving',
       ],
     }),
   ) as Promise<AMapModule>;
